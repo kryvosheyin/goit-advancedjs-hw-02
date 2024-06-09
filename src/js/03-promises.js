089f5a442fec26eb5ab01f9cebc5f50a61f9c733
kryvosheyin <alex.kryvosheyin@gmail.com>
2024-06-09 10:04:26 +0100

    homework completed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
-function createPromise(position, delay) {
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+const createPromise = (position, delay) => {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+};
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const firstDelay = parseInt(form.elements.delay.value);
+  const delayStep = parseInt(form.elements.step.value);
+  const promiseAmount = parseInt(form.elements.amount.value);
+
+  let timeout = firstDelay;
+  for (let i = 1; i <= promiseAmount; i++) {
+    createPromise(i, timeout)
+      .then(({ position, delay }) => {
+        iziToast.show({
+          title: `✅ Fulfilled promise ${position} in ${delay}ms`,
+          position: 'topRight',
+          backgroundColor: 'green',
+          titleColor: 'white',
+          close: false,
+        });
+      })
+      .catch(({ position, delay }) => {
+        iziToast.show({
+          title: `❌ Rejected promise ${position} in ${delay}ms`,
+          position: 'topRight',
+          backgroundColor: 'red',
+          titleColor: 'white',
+          close: false,
+        });
+      });
+    timeout += delayStep;
   }
-}
+});
